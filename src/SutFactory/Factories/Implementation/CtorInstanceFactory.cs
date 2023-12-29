02be415ad859cea43eb49dbf80665e62b28da6e2
Henrik Jensen <code@henrikjensen.info>
2023-12-29 15:25:32 +0100

    Add file header

--- src/SutFactory/Factories/Implementation/CtorInstanceFactory.cs
+++ src/SutFactory/Factories/Implementation/CtorInstanceFactory.cs
@@ -1,4 +1,20 @@
-﻿namespace Hj.SutFactory.Factories.Implementation;
+// <copyright file="CtorInstanceFactory.cs" company="Henrik Jensen">
+// Copyright 2024 Henrik Jensen
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+namespace Hj.SutFactory.Factories.Implementation;
 
 public sealed class CtorInstanceFactory(IServiceProvider serviceProvider) : FactoryBase, ICtorInstanceFactory
 {
@@ -6,11 +22,8 @@ public sealed class CtorInstanceFactory(IServiceProvider serviceProvider) : Fact
 
   public object Create(Type type)
   {
-    var ctor = GetConstructorInfo(type);
-    if (ctor == null)
-    {
-      throw new InvalidOperationException($"Cannot create instance '{type.AssemblyQualifiedName}' due to missing or ambiguous constructors.");
-    }
+    var ctor = GetConstructorInfo(type)
+      ?? throw new InvalidOperationException($"Cannot create instance '{type.AssemblyQualifiedName}' due to missing or ambiguous constructors.");
 
     var parameters = GetConstructorParameters(ctor, _serviceProvider);
 
